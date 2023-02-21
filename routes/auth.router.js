3deeb0b4f8031a739f3082a3cb2332ef848fc6e5
mrandycode <cevallos.andy@gmail.com>
2023-02-21 14:12:52 -0400

    Adjust for Veterinary Rol

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,49 +1,49 @@
-const express = require('express');
-const http = require('http');
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const { newPasswordSchema } = require('../schemas/auth.schema');
-const validatorHandler = require('./../middlewares/validator.handler');
-const constants = require('../shared/constants');
-const { config } = require('../config/config');
-const UserService = require('../services/user.service');
-const { checkApiKey } = require('../middlewares/auth.handler');
+const express = require("express");
+const http = require("http");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const { newPasswordSchema } = require("../schemas/auth.schema");
+const validatorHandler = require("./../middlewares/validator.handler");
+const constants = require("../shared/constants");
+const { config } = require("../config/config");
+const UserService = require("../services/user.service");
+const { checkApiKey } = require("../middlewares/auth.handler");
 const router = express.Router();
 const service = new UserService();
 
-router.post('/login',
-  passport.authenticate('local', { session: false }),
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
   async (req, res, next) => {
     try {
-
       const user = req.user;
       const payload = {
         sub: user.id,
-        role: user.role
-      }
-      const token = jwt.sign(payload, config.jwtSecret, { expiresIn: '30min' });
+        role: user.role,
+      };
+      const token = jwt.sign(payload, config.jwtSecret, { expiresIn: "30min" });
       delete user.dataValues.recoveryToken;
       await validateIsDoctor(req, res, user, token);
-
     } catch (error) {
       next(error);
     }
-  });
+  }
+);
 
-router.post('/change-password',
+router.post(
+  "/change-password",
   checkApiKey,
-  validatorHandler(newPasswordSchema, 'body'),
+  validatorHandler(newPasswordSchema, "body"),
   async (req, res, next) => {
     try {
       const { recoveryToken, password } = req.body;
 
       const response = await service.changePassword(recoveryToken, password);
       if (!response) {
-        res.status(201).json({ message: req.t('CHANGE_PASSWORD_SUCCESS') });
+        res.status(201).json({ message: req.t("CHANGE_PASSWORD_SUCCESS") });
       } else {
-        res.status(401).json({ message: req.t('UNAUTHORIZED') });
+        res.status(401).json({ message: req.t("UNAUTHORIZED") });
       }
-
     } catch (error) {
       next(error);
     }
@@ -52,36 +52,37 @@ router.post('/change-password',
 
 const validateIsDoctor = async (req, res, user, token) => {
   let isDoctorNew = false;
-  
-  if (user.role === 'doctor') {
+  if (constants.HEALTH_ROLES.find((role) => role === user.role)) {
     let doctorProfile = {};
     let options = constants.DOCTOR_ROUTER;
     const userId = user.id;
-    const path = '/api-core-rest/doctor-profile/user-id/';
+    const path = "/api-core-rest/doctor-profile/user-id/";
     options.headers.Authorization = `Bearer ${token}`;
-    options.path = `${path}${userId}`
+    options.path = `${path}${userId}`;
 
-    await http.get(options, function (response) {
-      let body = '';
-      response.on('data', function (chunk) {
-        body += chunk;
-      });
-      response.on('end', () => {
-        doctorProfile = JSON.parse(body);
-        if (doctorProfile && doctorProfile.id > 0) {
-          isDoctorNew = false;
-        } else {
-          isDoctorNew = true;
-        }
-        res.json({ user, token, isDoctorNew });
-      });
-    }).on('error', function (e) {
-      console.warn("Got error: " + e.message);
-    }).end();
-
+    await http
+      .get(options, function (response) {
+        let body = "";
+        response.on("data", function (chunk) {
+          body += chunk;
+        });
+        response.on("end", () => {
+          doctorProfile = JSON.parse(body);
+          if (doctorProfile && doctorProfile.id > 0) {
+            isDoctorNew = false;
+          } else {
+            isDoctorNew = true;
+          }
+          res.json({ user, token, isDoctorNew });
+        });
+      })
+      .on("error", function (e) {
+        console.warn("Got error: " + e.message);
+      })
+      .end();
   } else {
     res.json({ user, token, isDoctorNew });
   }
-}
+};
 
 module.exports = router;
--- shared/constants.js
+++ shared/constants.js
@@ -1,40 +1,41 @@
-const { config } = require('../config/config');
+const { config } = require("../config/config");
 
 module.exports = Object.freeze({
-    ORM_VALIDATION: [
-        {
-            path: 'profile_idx_02',
-            validatorKey: 'not_unique',
-            translateKey: 'PIN_ID_UNIQUE'
-        },
-        {
-            path: 'email',
-            validatorKey: 'not_unique',
-            translateKey: 'EMAIL_UNIQUE'
-        }
-    ],
-    EMAIL_RECOVERY: {
-        host: config.hostEmail,
-        port: config.portEmail,
-        path: '/api-send-mail-rest/auth/recovery/password',
-        method: 'POST',
-        headers: {
-            'api-key': config.apiKey,
-            'Content-Type': 'application/json'
-        }
+  ORM_VALIDATION: [
+    {
+      path: "profile_idx_02",
+      validatorKey: "not_unique",
+      translateKey: "PIN_ID_UNIQUE",
     },
-    DOCTOR_ROUTER: {
-        host: 'localhost',
-        port: 3000,
-        path: '/api-core-rest/doctor-profile/user-id/',
-        method: 'GET',
-        headers: {
-            'api-key': config.apiKey,
-            'Content-Type': 'application/json'
-        }
+    {
+      path: "email",
+      validatorKey: "not_unique",
+      translateKey: "EMAIL_UNIQUE",
     },
-    EMAILS: {
-        RECOVERY: "recovery@salvameid.com",
-        SUPPORT: "support@salvameid.com"
-    }
+  ],
+  EMAIL_RECOVERY: {
+    host: config.hostEmail,
+    port: config.portEmail,
+    path: "/api-send-mail-rest/auth/recovery/password",
+    method: "POST",
+    headers: {
+      "api-key": config.apiKey,
+      "Content-Type": "application/json",
+    },
+  },
+  DOCTOR_ROUTER: {
+    host: "localhost",
+    port: 3000,
+    path: "/api-core-rest/doctor-profile/user-id/",
+    method: "GET",
+    headers: {
+      "api-key": config.apiKey,
+      "Content-Type": "application/json",
+    },
+  },
+  EMAILS: {
+    RECOVERY: "recovery@salvameid.com",
+    SUPPORT: "support@salvameid.com",
+  },
+  HEALTH_ROLES: ["doctor", "veterinary"],
 });
